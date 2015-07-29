db6ee0b995ddbcd61777c9df9d767e083e33633d
voodoobettie <ingrid@frankenmedia.ca>
2015-07-29 11:02:49 -0700

    Added username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729153519) do
+ActiveRecord::Schema.define(version: 20150729173653) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name",       default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150729153519) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username",               default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
