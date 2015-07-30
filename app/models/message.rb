608a570fce4f29af74514d959304cf9cbf3344a6
voodoobettie <ingrid@frankenmedia.ca>
2015-07-29 22:47:26 -0700

    fixing more things

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
 
 # GET /messages/new
   def new
-    @message = Message.new
+    @message = new
     validates_presence_of :message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  get 'welcome/index'
+
   devise_for :users, controllers: {registrations: "registrations"}
   resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
